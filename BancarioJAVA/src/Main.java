6b6d46a8074cd6ed2efc48f3282b3aea2ab57cb6
DIOGO PATRICIO DONNER <diogo.donner@alunos.sc.senac.br>
2025-04-16 19:38:44 -0300

    Função 2 adicionada ao sistema

--- BancarioJAVA/src/Main.java
+++ BancarioJAVA/src/Main.java
@@ -2,12 +2,15 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
+import static java.lang.Thread.sleep;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+
         ArrayList<Integer> Bank = new ArrayList<>();
         ArrayList<String> account = new ArrayList<>();
         boolean active = true;
@@ -24,20 +27,43 @@ public class Main {
                     "6 - Transferir entre Contas\n" +
                     "7 - Sair");
             String option = scanner.nextLine();
-            if (!option.equals("1") && !option.equals("2") && !option.equals("3") && !option.equals("4") && !option.equals("5") && !option.equals("6") && !option.equals("7")) {
-                System.out.println("Operação não reconhecida");
-            } else {
+
 
                 switch (option) {
+                    case "debug":
+                        System.out.println("===Dev View===");
+                        System.out.println(Bank);
+                        System.out.println(account);
+                        System.out.println("===Dev View===");
+                        break;
                     case "1":
                         System.out.println("===Gerenciador Bancário===");
                         System.out.println("Digite o nome da nova conta:");
                         String name = scanner.nextLine();
                         System.out.println("A conta " + name + " foi criada com sucesso!");
                         account.add(name);
+                        Bank.add(0);
+                        break;
+                    case "2":
+                        System.out.println("===Gerenciador Bancário===");
+                        System.out.println("Digite a posição da conta");
+                        System.out.println(account);
+                        String position = scanner.nextLine();
+                        System.out.println(account);
+                        int positback = Integer.parseInt(position)-1;
+                        System.out.println("Digite a quantia a ser depositada:");
+                        String quant = scanner.nextLine();
+                        int quantback = Integer.parseInt(quant);
+                        System.out.println("Processando...");
+                        Bank.set(positback, quantback);
+                        System.out.println("A quantia de " + quant + " foi depositada ");
+                        System.out.println(account.get(positback)+": "+(quantback));
                         break;
 
-                }
+
+
+
+
 
             }
         }
